f2e65e422f20774d8e519c03e274989d04709a74
lizhanpeng <lizhanpeng@zego.im>
2020-09-02 13:04:36 +0800

    feat: export defines.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import ZegoExpressEngine from './ZegoExpressEngine'
 import {ZegoSurfaceView, ZegoTextureView} from './ZegoRenderView'
-
+export * from './ZegoExpressDefines'
 export default ZegoExpressEngine;
 export {
     ZegoSurfaceView,
